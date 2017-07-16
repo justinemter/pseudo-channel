fc3e3209dd34733ddcf2405603ff7fce61aa79f1
Justin <justinemter@gmail.com>
2017-07-16 00:36:07 -0700

    Update pseudo_config.py

--- pseudo_config.py
+++ pseudo_config.py
@@ -2,3 +2,21 @@
 
 baseurl = 'http://media.home:32400'
 token = 'the-token'
+
+'''
+*
+* The increment value between scheduled shows. Let's say you want to reposition all shows to have a clean start time divisable by 15 (i.e. 12:30 or 12:45). Use the value "-1" to disregard.
+* 
+*
+'''
+timeGap = 15
+
+
+timeBetweenShows = -1
+
+'''
+*
+* If there is an overlap, then the overlapGap var in config will determine the next increment. If it is set to "15", then the show will will bump up to the next 15 minute interval past the hour. If it is set to 30, then it will find the next 30 minute interval past the hour to place the episode. Useful for keeping clean schedules.
+*
+'''
+overlapGap = 15
