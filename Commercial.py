005d6fdea242061b8802521ace14e2792a31507a
Justin Emter <justinemter@gmail.com>
2017-07-18 09:57:44 -0700

    Porting over to oop.

---
+++ Commercial.py
@@ -0,0 +1,8 @@
+
+import Media
+
+def Commericial(Media):
+
+	def __init__(self, sectionType, title, naturalStartTime, naturalEndTime, duration, dayOfWeek):
+
+		super().__init__(self, sectionType, title, naturalStartTime, naturalEndTime, duration, dayOfWeek)
